9796f363bcaaa3502abfdce404c9085027dd579b
Bharath Reddy <bharathreddy186@gmail.com>
2019-08-30 18:19:02 +0530

    committing the first react app project

---
+++ src/components/tourData.js
@@ -0,0 +1,30 @@
+export const tourData = [
+	{
+		id:1,
+		city: "New York",
+		img: "./img/newyork.jpg",
+		name: "New york Bridge tour",
+		info: "The most beautiful city in the world"
+	},
+	{
+		id: 2,
+		city: "Los Angels",
+		img: "./img/losangels.jpg",
+		name: "Loas Angels City Routes",
+		info: "the known name by every citizen"
+	},
+	{
+		id: 3,
+		city: "Bloomingtom",
+		img: "./img/bloomington.jpg",
+		name: "Bloomington business area",
+		info: "the most insurance companies located area"
+	},
+	{
+		id: 4,
+		city: "Georgia",
+		img: "./img/georgia.jpg",
+		name: "Georgio city",
+		info: "business analytics working area"
+	}
+]
